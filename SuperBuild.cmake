658f40c0c46c33a4b237f3dc98733956449be423
Raul San Jose <rjosest@gmail.com>
2014-03-03 16:49:43 -0500

    ENH: Adding SuperBuild initial template

---
+++ SuperBuild.cmake
@@ -0,0 +1,64 @@
+#-----------------------------------------------------------------------------
+# Enable and setup External project global properties
+#-----------------------------------------------------------------------------
+include(ExternalProject)
+set(ep_base        "${CMAKE_BINARY_DIR}")
+
+# Compute -G arg for configuring external projects with the same CMake generator:
+if(CMAKE_EXTRA_GENERATOR)
+  set(gen "${CMAKE_EXTRA_GENERATOR} - ${CMAKE_GENERATOR}")
+else()
+  set(gen "${CMAKE_GENERATOR}")
+endif()
+
+#-----------------------------------------------------------------------------
+# Build options
+#-----------------------------------------------------------------------------
+
+OPTION(SLICERCIP_ENABLE_EXPERIMENTAL_MODULES "Enable the building of work-in-progress, experimental modules." OFF)
+
+#-----------------------------------------------------------------------------
+# Project dependencies
+#-----------------------------------------------------------------------------
+
+##
+set(inner_DEPENDENCIES "CIP")
+
+SlicerMacroCheckExternalProjectDependency(inner)
+
+set(ep_cmake_args)
+foreach(dep ${EXTENSION_DEPENDS})
+  list(APPEND ep_cmake_args -D${dep}_DIR:PATH=${${dep}_DIR})
+#  message("--------------dep:${dep}")
+endforeach()
+
+set(proj inner)
+
+ExternalProject_Add(${proj}
+  DOWNLOAD_COMMAND ""
+  INSTALL_COMMAND ""
+  SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
+  BINARY_DIR ${EXTENSION_BUILD_SUBDIRECTORY}
+  CMAKE_GENERATOR ${gen}
+  CMAKE_ARGS
+    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+    -DCMAKE_RUNTIME_OUTPUT_DIRECTORY:PATH=${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+    -DCMAKE_LIBRARY_OUTPUT_DIRECTORY:PATH=${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
+    -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY:PATH=${CMAKE_ARCHIVE_OUTPUT_DIRECTORY}
+    -DCMAKE_CXX_COMPILER:STRING=${CMAKE_CXX_COMPILER}
+    -DADDITIONAL_C_FLAGS:STRING=${ADDITIONAL_C_FLAGS}
+    -DADDITIONAL_CXX_FLAGS:STRING=${ADDITIONAL_CXX_FLAGS}
+    -D${EXTENSION_NAME}_SUPERBUILD:BOOL=OFF
+    -DEXTENSION_SUPERBUILD_BINARY_DIR:PATH=${${EXTENSION_NAME}_BINARY_DIR}
+    -DSLICERCIP_ENABLE_EXPERIMENTAL_MODULES:BOOL=${SLICERCIP_ENABLE_EXPERIMENTAL_MODULES}
+    -DCIP_DIR:PATH=${SLICERCIP_CIP_DIR}
+    # Slicer
+    -DSlicer_DIR:PATH=${Slicer_DIR}
+    -DMIDAS_PACKAGE_EMAIL:STRING=${MIDAS_PACKAGE_EMAIL}
+    -DMIDAS_PACKAGE_API_KEY:STRING=${MIDAS_PACKAGE_API_KEY}
+    -DSubversion_SVN_EXECUTABLE:STRING=${Subversion_SVN_EXECUTABLE}
+    -DGIT_EXECUTABLE:STRING=${GIT_EXECUTABLE}
+    ${ep_cmake_args}
+  DEPENDS
+    ${${proj}_DEPENDENCIES}
+  )
