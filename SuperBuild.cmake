22f8e0e9bb8455f02a3d2fea3f64e15c43cf32bc
Raul San Jose <rjosest@gmail.com>
2015-01-08 19:12:08 -0500

    ENH: Using the Superbuild Extension mechanism to build CIP with SlicerCIP

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -1,64 +1,58 @@
-#-----------------------------------------------------------------------------
-# Enable and setup External project global properties
-#-----------------------------------------------------------------------------
-include(ExternalProject)
-set(ep_base        "${CMAKE_BINARY_DIR}")
 
-# Compute -G arg for configuring external projects with the same CMake generator:
-if(CMAKE_EXTRA_GENERATOR)
-  set(gen "${CMAKE_EXTRA_GENERATOR} - ${CMAKE_GENERATOR}")
-else()
-  set(gen "${CMAKE_GENERATOR}")
+#-----------------------------------------------------------------------------
+# Git protocol option
+#-----------------------------------------------------------------------------
+option(Slicer_USE_GIT_PROTOCOL "If behind a firewall turn this off to use http instead." ON)
+
+set(git_protocol "git")
+if(NOT Slicer_USE_GIT_PROTOCOL)
+  set(git_protocol "http")
 endif()
 
 #-----------------------------------------------------------------------------
-# Build options
+# Enable and setup External project global properties
 #-----------------------------------------------------------------------------
 
-OPTION(SLICERCIP_ENABLE_EXPERIMENTAL_MODULES "Enable the building of work-in-progress, experimental modules." OFF)
+set(ep_common_c_flags "${CMAKE_C_FLAGS_INIT} ${ADDITIONAL_C_FLAGS}")
+set(ep_common_cxx_flags "${CMAKE_CXX_FLAGS_INIT} ${ADDITIONAL_CXX_FLAGS}")
 
 #-----------------------------------------------------------------------------
 # Project dependencies
 #-----------------------------------------------------------------------------
 
-##
-set(inner_DEPENDENCIES "CIP")
+include(ExternalProject)
 
-#SlicerMacroCheckExternalProjectDependency(inner)
-
-set(ep_cmake_args)
 foreach(dep ${EXTENSION_DEPENDS})
-  list(APPEND ep_cmake_args -D${dep}_DIR:PATH=${${dep}_DIR})
-#  message("--------------dep:${dep}")
+  mark_as_superbuild(${dep}_DIR)
 endforeach()
 
-set(proj inner)
+set(proj ${SUPERBUILD_TOPLEVEL_PROJECT})
+set(${proj}_DEPENDS CIP)
+
+ExternalProject_Include_Dependencies(${proj}
+  PROJECT_VAR proj
+  SUPERBUILD_VAR ${EXTENSION_NAME}_SUPERBUILD
+  )
 
 ExternalProject_Add(${proj}
+  ${${proj}_EP_ARGS}
   DOWNLOAD_COMMAND ""
   INSTALL_COMMAND ""
   SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
   BINARY_DIR ${EXTENSION_BUILD_SUBDIRECTORY}
-  CMAKE_GENERATOR ${gen}
-  CMAKE_ARGS
+  CMAKE_CACHE_ARGS
+    -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
+    -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}
+    -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
+    -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}
     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
     -DCMAKE_RUNTIME_OUTPUT_DIRECTORY:PATH=${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
     -DCMAKE_LIBRARY_OUTPUT_DIRECTORY:PATH=${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
     -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY:PATH=${CMAKE_ARCHIVE_OUTPUT_DIRECTORY}
-    -DCMAKE_CXX_COMPILER:STRING=${CMAKE_CXX_COMPILER}
-    -DADDITIONAL_C_FLAGS:STRING=${ADDITIONAL_C_FLAGS}
-    -DADDITIONAL_CXX_FLAGS:STRING=${ADDITIONAL_CXX_FLAGS}
-    -D${EXTENSION_NAME}_SUPERBUILD:BOOL=OFF
-    -DEXTENSION_SUPERBUILD_BINARY_DIR:PATH=${${EXTENSION_NAME}_BINARY_DIR}
-    -DSLICERCIP_ENABLE_EXPERIMENTAL_MODULES:BOOL=${SLICERCIP_ENABLE_EXPERIMENTAL_MODULES}
-    -DCIP_DIR:PATH=${SLICERCIP_CIP_DIR}
-    # Slicer
-    -DSlicer_DIR:PATH=${Slicer_DIR}
     -DMIDAS_PACKAGE_EMAIL:STRING=${MIDAS_PACKAGE_EMAIL}
     -DMIDAS_PACKAGE_API_KEY:STRING=${MIDAS_PACKAGE_API_KEY}
-    -DSubversion_SVN_EXECUTABLE:STRING=${Subversion_SVN_EXECUTABLE}
-    -DGIT_EXECUTABLE:STRING=${GIT_EXECUTABLE}
-    ${ep_cmake_args}
+    -D${EXTENSION_NAME}_SUPERBUILD:BOOL=OFF
+    -DEXTENSION_SUPERBUILD_BINARY_DIR:PATH=${${EXTENSION_NAME}_BINARY_DIR}
   DEPENDS
-    ${${proj}_DEPENDENCIES}
+    ${${proj}_DEPENDS}
   )
