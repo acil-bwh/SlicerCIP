a670bc120682057453b20b8639a6bff47450dbaa
jonieva <jorgeonieva@gmail.com>
2016-05-05 10:12:22 -0400

    Remove Colors.py from init to avoid dependencies with Slicer

--- Scripted/CIP_Common/CIP/logic/__init__.py
+++ Scripted/CIP_Common/CIP/logic/__init__.py
@@ -3,6 +3,6 @@ from geometry_topology_data import *
 from EventsTrigger import *
 import file_conventions
 #from StructuresParameters import *
-from Colors import *
+#from Colors import *
 from timer import *
 
